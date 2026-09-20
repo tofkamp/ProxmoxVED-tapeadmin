ffdc4924153c6465f3b49a632bbd540d4ec84db3
Tjibbe Hofkamp <t_w_h@hotmail.com>
2026-09-20 16:16:01 +0200

    Update tapeadmin-install.sh

--- install/tapeadmin-install.sh
+++ install/tapeadmin-install.sh
@@ -13,34 +13,35 @@ setting_up_container
 network_check
 update_os
 
-fetch_and_deploy_gh_release "tapeadmin" "https://github.com/tofkamp/PASTA.git" "tarball"
+#fetch_and_deploy_gh_release "tapeadmin" "https://github.com/tofkamp/PASTA.git" "tarball"
 
 #msg_info "Installing Dependencies"
 $STD apt install -y python3-pip
 $STD apt install -y git
 $STD mkdir -p /opt/tapeadmin
 $STD git clone https://github.com/tofkamp/PASTA.git /opt/tapeadmin
+$STD chmod 700 /opt/tapeadmin
 #$STD pip install -r requirements.txt --break-system-packages
 #msg_ok "Installed Dependencies"
 
 msg_info "Setting up Python Environment"
-python3 -m venv /opt/venv
-/opt/venv/bin/pip install --upgrade pip >/dev/null 2>&1
+$STD python3 -m venv /opt/venv
+$STD /opt/venv/bin/pip install --upgrade pip >/dev/null 2>&1
 $STD /opt/venv/bin/pip install --no-cache-dir \
-proxmoxer \
-requests
+  proxmoxer \
+  requests
 msg_ok "Set up Python Environment"
 
 msg_info "Configuring TapeAdmin"
-cp config.example.ini config.ini
+$STD cp /opt/tapeadmin/config.example.ini /opt/tapeadmin/config.ini
 msg_ok "Configured TapeAdmin"
 
 msg_info "Creating Scheduled Tasks"
-cat <<'EOF' >/etc/cron.d/tapeadmin
+$STD cat <<'EOF' >/etc/cron.d/tapeadmin
 45 7 * * *  cd /opt/tapeadmin && ./tape_admin.py run        >> run.log 2>&1
 0 8   * * *   cd /opt/tapeadmin && ./tape_admin.py check-overdue >> run.log 2>&1
 EOF
-chmod 644 /etc/cron.d/tapeadmin
+$STD chmod 644 /etc/cron.d/tapeadmin
 msg_ok "Created Scheduled Tasks"
 
 motd_ssh
